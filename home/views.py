ce32f5d01352abd615d882701148522dc2672cd4
Lalith <lalithgunnu2202@gmail.com>
2025-06-06 12:33:04 +0530

    product details can be viewed from homepage fully

--- home/views.py
+++ home/views.py
@@ -5,6 +5,7 @@ from .models import *
 # Create your views here.
 def index(request):
     prods = Product.objects.all()[:4]
+    eight_prods = Product.objects.all()[:8]
     cats = Type.objects.all()
     # count = Cart_item.objects.filter(user=request.user).count()
-    return render(request,'homepage.html',{'prods':prods,'cats':cats})
+    return render(request,'homepage.html',{'prods':prods,'cats':cats,'eight_prods':eight_prods})
